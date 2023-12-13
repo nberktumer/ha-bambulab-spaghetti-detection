b15982d50c394429d52f7fa842098c9c8433959f
nberktumer <naimberktumer@gmail.com>
2023-12-13 21:57:01 +0300

    make the port number configurable

--- addon/run.sh
+++ addon/run.sh
@@ -2,5 +2,6 @@
 set -e
 
 ML_API_TOKEN=$(bashio::config 'obico_api_secret')
+PORT=$(bashio::addon.port 3333)
 
-venv/bin/gunicorn --bind 0.0.0.0:3333 --workers 1 wsgi
+venv/bin/gunicorn --bind "0.0.0.0:$PORT" --workers 1 wsgi
